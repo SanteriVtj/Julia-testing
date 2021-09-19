153c780de5f7c13ad16f2dc3ee8e5c1a4e69f814
SanteriVtj <santeri.vaataja@gmail.com>
2021-09-19 06:59:16 +0300

    FOC and function plotted

--- elementary_optimization.jl
+++ elementary_optimization.jl
@@ -1,8 +1,12 @@
-using Plots, Roots, ForwardDiff
+using Plots, Roots, ForwardDiff, LaTeXStrings
 
 f(x) = 2x^4 - 5x^3 - x^2
 
 D(f) = x -> ForwardDiff.derivative(f, float(x))
 
-plot(-3:.01:3, f.(-3:.01:3))
-find_zero(D, (-3, 3))
+α = find_zeros(D(f), (-3, 3))
+
+plot(-3:.01:3, f.(-3:.01:3), label=L"2x^4-5x^3-x^2")
+plot!(α, f.(α), seriestype=:scatter, label="Roots")
+
+
